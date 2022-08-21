c82d73343fd23133da600f2bc32160d0b214109b
Aung Myo Kyaw <aungmyokyaw.git@icloud.com>
2022-08-21 23:27:11 +0630

    feat: clean fb news feed

---
+++ README.md
@@ -0,0 +1,34 @@
+# clean fb newsfeed
+
+- fb ကို logout လည်း လုပ် မထား ချင်ဘူး ။
+- သူများ တင် တဲ့ post ကို လည်း မကြည့်ချင်ဘူး ။
+- ကိုယ် ပဲ တင် ချင်တယ်
+- safari ပဲ သုံး ချင်တယ်
+
+_ထို့ကြောင့်_
+
+---
+
+- install [ userscripts extension ](https://apps.apple.com/us/app/userscripts/id1463298887) first
+- install [ clean fb news feed scripts ](#)
+
+---
+
+သူများ တင် တဲ့ post ပြန် ကြည့်ချင် ရင် အဲ့တာကို ပိတ် လိုက် ပေါ့ ။
+oki
+
+---
+
+readme file was edited using sublimetext
+
+[ဒီနေရာမှာဖြင့် မ vim နိုင်တော့ပါ]
+
+---
+
+## demo ss
+
+<img src="./assets/clean-newsfeed.png" align="center">
+
+## License
+
+MIT © [Aung Myo Kyaw](https://github.com/AungMyoKyaw)
---
+++ clean-fb-newsfeed.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         Clean Facebook Newsfeed
+// @version      0.1
+// @description  Clean Facebook Newsfeed
+// @author       aungmyokyaw
+// @match        https://www.facebook.com/
+// @grant        none
+// @run-at document-end
+// ==/UserScript==
+
+function hideFeed() {
+  var feeds = document.querySelectorAll("div[role=feed]");
+  for (var i = 0; i < feeds.length; i++) {
+    var feed = feeds[i];
+    feed.innerHTML = "";
+  }
+}
+
+function addRandomQuoteForHiddenPost() {
+  var feeds = document.querySelectorAll("div[role=article]");
+  for (var i = 0; i < feeds.length; i++) {
+    var feed = feeds[i];
+    var feedTEXT = feeds[i].innerText;
+    // Something went wrong
+    // This may be because of a technical error that we're working to get fixed. Try reloading this page.
+    // Reload Page
+    if (feedTEXT.includes("Something went wrong")) {
+      feed.innerHTML =
+        "<div style='display: flex; align-items: center; justify-content: center; height: 15em;'><div style='color: white; font-size: 3em;'>" +
+        "❄️" +
+        "</div></div>";
+    }
+  }
+}
+
+// Select the node that will be observed for mutations
+const targetNode = document.querySelector("body");
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    hideFeed();
+    addRandomQuoteForHiddenPost();
+  }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
